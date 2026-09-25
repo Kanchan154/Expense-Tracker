c3b5717f072af04936d6f952427377109765a762
Kanchan Mishra <133621440+Kanchan154@users.noreply.github.com>
2026-09-25 20:39:00 +0530

    Enhance index.js with comments for clarity
    Added comments for middleware and API routes.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from "cors"
 import { intializeDatabase } from "./config/db.js";
 dotenv.config();
 const app = express()
+// middleware to accept req.body;
 app.use(express.json())
 app.use(cors())
 const PORT = process.env.PORT
@@ -11,10 +12,11 @@ const PORT = process.env.PORT
 import AuthRouter from "./routes/user.route.js";
 import TransactionRouter from "./routes/transaction.route.js";
 
-app.use('/api/auth', AuthRouter);
-app.use('/api/transaction', TransactionRouter);
+app.use('/api/auth', AuthRouter);         // api for authentication
+app.use('/api/transaction', TransactionRouter);        // api for transactions
 
 app.listen(PORT, () => {
+    // initialize db
     intializeDatabase();
     console.log(`Server is listening to port ${PORT}`)
 })
